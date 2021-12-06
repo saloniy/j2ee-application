12337f4fc8c8ee7a22ec816e9f088c6c62a9bba9
Saloni Yadav <saloniy97@gmail.com>
2021-12-06 12:51:27 -0500

    Added add product implementation; session check; declaration in each file; extracted common code

--- src/main/java/controller/ProductsContoller.java
+++ src/main/java/controller/ProductsContoller.java
@@ -1,16 +1,32 @@
+/*********************************************************************************
+* ITE5332 : Project
+* I declare that this assignment is my own work in accordance with Humber Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Saloni Yadav, Preeti Kshirsagar; Student ID: N01414159, N01494576  Date: 6 Dec, 2021
+*
+********************************************************************************/
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.DBConn;
+import dao.ProductQueries;
+import model.Products;
+
 /**
  * Servlet implementation class ProductsContoller
  */
-@WebServlet("/ProductsContoller")
+@WebServlet("/addProduct")
 public class ProductsContoller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,16 +42,52 @@ public class ProductsContoller extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			ProductQueries productQuery = new ProductQueries(DBConn.getConnection());	
+			ArrayList<Products> products = productQuery.getAllProducts();
+			RequestDispatcher rd = request.getRequestDispatcher("/add_products_form.jsp");
+			request.setAttribute("products", products);
+			rd.forward(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		RequestDispatcher rd = request.getRequestDispatcher("/add_products_form.jsp");
+		ProductQueries productQuery = null;
+		try {
+			productQuery = new ProductQueries(DBConn.getConnection());
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}	
+		ArrayList<Products> products = productQuery.getAllProducts();
+		if(request.getParameter("productName") == "") {
+			request.setAttribute("error", "Please enter a valid product name");
+			request.setAttribute("products", products);
+			rd.forward(request, response);
+			return;
+		}
+		try {
+			productQuery = new ProductQueries(DBConn.getConnection());	
+			products = productQuery.addNewProduct(request.getParameter("productName"));
+			if(products != null) {
+				request.setAttribute("products", products);
+			} else {
+				request.setAttribute("error", "Some error occurred. Please try again");
+			}
+			rd.forward(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/webapp/view_claims.jsp
+++ src/main/webapp/view_claims.jsp
@@ -1,12 +1,19 @@
+<!--/*********************************************************************************
+* ITE5332 : Project
+* I declare that this assignment is my own work in accordance with Humber Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Saloni Yadav, Preeti Kshirsagar; Student ID: N01414159, N01494576; Date: ____________________
+*
+********************************************************************************/-->
+<%
+	if(request.getSession().getAttribute("username") == null) {
+		response.sendRedirect(request.getContextPath() + "/login");
+	}
+%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
-</html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+   	<jsp:include page="header.jsp"></jsp:include>
+   	<jsp:include page="footer.jsp"></jsp:include>

743ee2b44feb95f405c4ab0ea508eb157b588dcf
Saloni Yadav <saloniy97@gmail.com>
2021-12-12 15:48:12 -0500

    added view claims button to all with claims

--- src/main/java/controller/ClaimsController.java
+++ src/main/java/controller/ClaimsController.java
@@ -63,12 +63,4 @@ public class ClaimsController extends HttpServlet {
 		rd.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
